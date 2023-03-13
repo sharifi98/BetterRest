5a685c2c991b9a023ad3c4ac5e12b8c9b3371bf7
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-13 13:16:18 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hossein Sharifi on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
